05bf795384efdd2038e7c6cda1eaac128267e21b
Ostap Haivanovych <97313368+9uyone@users.noreply.github.com>
2025-11-09 21:40:02 +0200

    Project structure

---
+++ src/Gateway/Program.cs
@@ -0,0 +1,54 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.MapOpenApi();
+}
+
+//app.UseHttpsRedirection();
+
+app.Use(async (context, next) => {
+	var path = context.Request.Path.Value!;
+	if (path.Length > 1 && path.EndsWith("/"))
+		context.Request.Path = path.TrimEnd('/');
+	await next();
+});
+
+
+await app.UseOcelot();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () => {
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary) {
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
